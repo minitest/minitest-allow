a57a86ce1b930bfee4d3c7b47efb9362ecf1f8ae
Ryan Davis <ryand-ruby@zenspider.com>
2022-05-12 15:13:13 -0800

    ! The allow file allows for regular expressions to match error messages by pattern. + Explicity use psych. Don't ask. + Use safe_load_file if it is available and allow Regexps. + Output of good/bad tests is now in YAML format with easily searchable headers.
    [git-p4: depot-paths = "//src/minitest-allow/dev/": change = 13375]

--- lib/minitest/allow_plugin.rb
+++ lib/minitest/allow_plugin.rb
@@ -3,13 +3,18 @@ module Minitest
     @allow = @allow_save = false
 
     opts.on "-a", "--allow=path", String, "Allow listed tests to fail." do |f|
-      require "yaml"
-      @allow = YAML.load_file(f)
-        .map { |s| String === s && s.start_with?("/") ? Regexp.new(s[1..-2]) : s }
+      # don't ask why I'm using this specifically:
+      require "psych"
+
+      @allow = if Psych.respond_to? :safe_load_file then
+                 Psych.safe_load_file f, permitted_classes: [Regexp]
+               else
+                 Psych.load_file f
+               end
     end
 
     opts.on "-A", "--save-allow=path", String, "Save failing tests." do |f|
-      require "yaml"
+      require "psych"
       @allow_save = f
     end
   end
@@ -58,12 +63,28 @@ module Minitest
         .uniq
         .sort
 
-      File.write allow_save, data.to_yaml
+      File.write allow_save, Psych.dump(data, line_width:-1)
     end
 
-    def filter_allow
-      allow_results = self.allow_results
+    # Test runs call record, we put everything ran in allow_seen.
+    # This means allow_seen has everything RAN, regardless of pass/fail
+    # We use this intersected with the allowed file to determine what to report
+    # on... if it wasn't seen, we don't say add/remove at all.
+    #
+    # `allow` is all the stuff from the allow file. Test names and
+    # regexps that excuse failures.
+    #
+    # `allow_results` is an array of arrays of results: [[result, ...], ...]
+    #
+    # When the run is done, we want to tell the user what tests to
+    # remove or add from the allowed list:
+    #
+    # * If a test passed that is in the allowed list, it should be removed.
+    # * If a test failed that is NOT in the allowed list, it should be added.
+    # * If a test failed that is matched by a regexp, it's NAME should be removed
+    #   if it is listed.
 
+    def filter_allow
       # 1. split allow into strings and regexps
       allowed_REs, allowed_names = allow.partition { |a| Regexp === a }
 
@@ -71,43 +92,71 @@ module Minitest
 
       # 2. remove items from allow_results whose full_name matches the strings
       # 3. remove items from allow_results whose message matches the regexps
+      # 4. remove items from allow_results whose full_name matches the regexps?
+
       hit = {}
+      allow_results = self.allow_results
       allow_results.each do |results|
         results.delete_if { |r|
-          x = (allowed_names[r.full_name] ||
-               allowed_REs.find { |re| r.failure.message =~ re })
+          name = r.full_name
 
-          hit[x] = true if x
+          by_name = allowed_names[name]
+          by_regx = allowed_REs.find { |re| r.failure.message =~ re || name =~ re }
+
+          # this will add the name as bad unless hit by regexp as well
+          # if hit by regex, then we want to report it as "good" so
+          # the name gets removed from the allow list:
+
+          hit[name] = true if by_name && !by_regx
+
+          by_name || by_regx
         }
       end
 
-      # 4. remove string and regexps that matched any of the above from allow
+      # 5. remove string and regexps that matched any of the above from allow
       self.allow -= hit.keys
 
-      extra_bad = allow_results.flatten.map(&:full_name)
+      errored, failed = allow_results
+        .flatten
+        .partition { |t| Minitest::UnexpectedError === t.failure }
+
+      failed = failed.map(&:full_name)
+      errors = Hash.new { |h,k| h[k] = [] }
+
+      errored.each do |t|
+        msg = t.failure.message.lines.first.chomp.gsub(/0x\h+/, "0xHHHH")
+
+        errors[Regexp.new(Regexp.escape(msg))] << t.full_name
+      end
+
+      extra_bad = failed.uniq
+      extra_bad << errors.transform_values(&:uniq) unless errors.empty?
+
+      # 6. report new failures including regular expressions for errors
+
       unless extra_bad.empty? then
         io.puts
         io.puts "Bad tests that are NOT allowed:"
+        Psych.dump extra_bad, io, line_width:-1
         io.puts
-        io.puts extra_bad.to_yaml
       end
     end
 
     def report_extra_allow
-      unless allow.empty? then
+      good = allow & allow_seen
+
+      unless good.empty? then
         io.puts
         io.puts "Excluded tests that now pass:"
+        Psych.dump good, io, line_width:-1
         io.puts
-        allow.each do |name|
-          io.puts "  :allow_good: %p" % [name]
-        end
       end
     end
 
     def passed?
       write_allow        if allow_save
       filter_allow       if allow
-      report_extra_allow if allow_save
+      report_extra_allow if allow
 
       super # CompositeReporter#passed?
     end
